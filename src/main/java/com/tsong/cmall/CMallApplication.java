3a92d91ec2ae4098208b17aa199771df9633f9ab
yaocong <rogwel@vip.qq.com>
2023-03-20 02:42:40 +0800

    自动生成代码

--- src/main/java/com/tsong/cmall/CMallApplication.java
+++ src/main/java/com/tsong/cmall/CMallApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CMallApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CMallApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CMallApplication.class, args);
+    }
 
 }
