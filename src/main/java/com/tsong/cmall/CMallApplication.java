f1bede4ef1f8cf02ee045cf7141d5511737e97bf
yaocong <rogwel@vip.qq.com>
2023-03-19 18:12:24 +0800

    Initial commit

---
+++ src/main/java/com/tsong/cmall/CMallApplication.java
@@ -0,0 +1,13 @@
+package com.tsong.cmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsong/cmall/CMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsong.cmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
