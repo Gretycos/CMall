8e6adfab6683d7cc66e062f23d780b5f8bd52d6b
yaocong <rogwel@vip.qq.com>
2023-03-24 23:29:51 +0800

    ShoppingCartItem

--- src/main/java/com/tsong/cmall/dao/ShoppingCartItemMapper.java
+++ src/main/java/com/tsong/cmall/dao/ShoppingCartItemMapper.java
@@ -2,6 +2,8 @@ package com.tsong.cmall.dao;
 
 import com.tsong.cmall.entity.ShoppingCartItem;
 
+import java.util.List;
+
 /**
  * @Author: Tsong
  * @date: 2023/03/20/09:31
@@ -18,4 +20,12 @@ public interface ShoppingCartItemMapper {
     int updateByPrimaryKeySelective(ShoppingCartItem row);
 
     int updateByPrimaryKey(ShoppingCartItem row);
+
+    ShoppingCartItem selectByUserIdAndGoodsId(Long mallUserId, Long goodsId);
+
+    List<ShoppingCartItem> selectByUserId(Long mallUserId, int number);
+
+    int selectCountByUserId(Long mallUserId);
+
+    int deleteBatch(List<Long> ids);
 }
