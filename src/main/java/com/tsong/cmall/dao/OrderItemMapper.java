853397f765f6ff96225d9fcdbed28498627f339d
yaocong <rogwel@vip.qq.com>
2023-03-24 23:29:29 +0800

    OrderItem

--- src/main/java/com/tsong/cmall/dao/OrderItemMapper.java
+++ src/main/java/com/tsong/cmall/dao/OrderItemMapper.java
@@ -2,6 +2,8 @@ package com.tsong.cmall.dao;
 
 import com.tsong.cmall.entity.OrderItem;
 
+import java.util.List;
+
 /**
  * @Author: Tsong
  * @date: 2023/03/20/09:31
@@ -18,4 +20,28 @@ public interface OrderItemMapper {
     int updateByPrimaryKeySelective(OrderItem row);
 
     int updateByPrimaryKey(OrderItem row);
+
+    /**
+     * 根据订单id获取订单项列表
+     *
+     * @param orderId
+     * @return
+     */
+    List<OrderItem> selectByOrderId(Long orderId);
+
+    /**
+     * 根据订单ids获取订单项列表
+     *
+     * @param orderIds
+     * @return
+     */
+    List<OrderItem> selectByOrderIds(List<Long> orderIds);
+
+    /**
+     * 批量insert订单项数据
+     *
+     * @param orderItems
+     * @return
+     */
+    int insertBatch(List<OrderItem> orderItems);
 }
