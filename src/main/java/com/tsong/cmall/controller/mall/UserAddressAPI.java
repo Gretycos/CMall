c33bba18d126b1933e854487e94fdda67df12a51
yaocong <rogwel@vip.qq.com>
2023-04-03 00:03:53 +0800

    UserAddress

---
+++ src/main/java/com/tsong/cmall/controller/mall/UserAddressAPI.java
@@ -0,0 +1,109 @@
+package com.tsong.cmall.controller.mall;
+
+import com.tsong.cmall.common.ServiceResultEnum;
+import com.tsong.cmall.controller.mall.param.SaveUserAddressParam;
+import com.tsong.cmall.controller.mall.param.UpdateUserAddressParam;
+import com.tsong.cmall.controller.vo.UserAddressVO;
+import com.tsong.cmall.entity.MallUser;
+import com.tsong.cmall.entity.UserAddress;
+import com.tsong.cmall.service.UserAddressService;
+import com.tsong.cmall.util.BeanUtil;
+import com.tsong.cmall.util.Result;
+import com.tsong.cmall.util.ResultGenerator;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Author Tsong
+ * @Date 2023/4/2 23:38
+ */
+@RestController
+@Api(value = "User Address", tags = "6.商城个人地址相关接口")
+@RequestMapping("/api")
+public class UserAddressAPI {
+    @Autowired
+    private UserAddressService userAddressService;
+
+    @GetMapping("/address")
+    @ApiOperation(value = "我的收货地址列表", notes = "")
+    public Result<List<UserAddressVO>> addressList(@TokenToMallUser MallUser loginMallUser) {
+        return ResultGenerator.genSuccessResult(userAddressService.getMyAddresses(loginMallUser.getUserId()));
+    }
+
+    @PostMapping("/address")
+    @ApiOperation(value = "添加地址", notes = "")
+    public Result<Boolean> saveUserAddress(@RequestBody SaveUserAddressParam saveUserAddressParam,
+                                           @TokenToMallUser MallUser loginMallUser) {
+        UserAddress userAddress = new UserAddress();
+        BeanUtil.copyProperties(saveUserAddressParam, userAddress);
+        userAddress.setUserId(loginMallUser.getUserId());
+        Boolean saveResult = userAddressService.saveUserAddress(userAddress);
+        //添加成功
+        if (saveResult) {
+            return ResultGenerator.genSuccessResult();
+        }
+        //添加失败
+        return ResultGenerator.genFailResult("添加失败");
+    }
+
+    @PutMapping("/address")
+    @ApiOperation(value = "修改地址", notes = "")
+    public Result<Boolean> updateUserAddress(@RequestBody UpdateUserAddressParam updateUserAddressParam,
+                                                 @TokenToMallUser MallUser loginMallUser) {
+        UserAddress userAddressFromDB = userAddressService.getUserAddressById(updateUserAddressParam.getAddressId());
+        if (!loginMallUser.getUserId().equals(userAddressFromDB.getUserId())) {
+            return ResultGenerator.genFailResult(ServiceResultEnum.REQUEST_FORBIDDEN_ERROR.getResult());
+        }
+        UserAddress userAddress = new UserAddress();
+        BeanUtil.copyProperties(updateUserAddressParam, userAddress);
+        userAddress.setUserId(loginMallUser.getUserId());
+        Boolean updateResult = userAddressService.updateUserAddress(userAddress);
+        //修改成功
+        if (updateResult) {
+            return ResultGenerator.genSuccessResult();
+        }
+        //修改失败
+        return ResultGenerator.genFailResult("修改失败");
+    }
+
+    @GetMapping("/address/{addressId}")
+    @ApiOperation(value = "获取收货地址详情", notes = "传参为地址id")
+    public Result<UserAddressVO> getUserAddress(@PathVariable("addressId") Long addressId,
+                                                              @TokenToMallUser MallUser loginMallUser) {
+        UserAddress userAddress = userAddressService.getUserAddressById(addressId);
+        UserAddressVO userAddressVO = new UserAddressVO();
+        BeanUtil.copyProperties(userAddress, userAddressVO);
+        if (!loginMallUser.getUserId().equals(userAddress.getUserId())) {
+            return ResultGenerator.genFailResult(ServiceResultEnum.REQUEST_FORBIDDEN_ERROR.getResult());
+        }
+        return ResultGenerator.genSuccessResult(userAddressVO);
+    }
+
+    @GetMapping("/address/default")
+    @ApiOperation(value = "获取默认收货地址", notes = "无传参")
+    public Result getDefaultUserAddress(@TokenToMallUser MallUser loginMallUser) {
+        UserAddress mallUserAddressById = userAddressService.getMyDefaultAddressByUserId(loginMallUser.getUserId());
+        return ResultGenerator.genSuccessResult(mallUserAddressById);
+    }
+
+    @DeleteMapping("/address/{addressId}")
+    @ApiOperation(value = "删除收货地址", notes = "传参为地址id")
+    public Result deleteAddress(@PathVariable("addressId") Long addressId,
+                                @TokenToMallUser MallUser loginMallUser) {
+        UserAddress mallUserAddressById = userAddressService.getUserAddressById(addressId);
+        if (!loginMallUser.getUserId().equals(mallUserAddressById.getUserId())) {
+            return ResultGenerator.genFailResult(ServiceResultEnum.REQUEST_FORBIDDEN_ERROR.getResult());
+        }
+        Boolean deleteResult = userAddressService.deleteById(addressId);
+        //删除成功
+        if (deleteResult) {
+            return ResultGenerator.genSuccessResult();
+        }
+        //删除失败
+        return ResultGenerator.genFailResult(ServiceResultEnum.OPERATE_ERROR.getResult());
+    }
+}
---
+++ src/main/java/com/tsong/cmall/controller/mall/param/UpdateUserAddressParam.java
@@ -0,0 +1,40 @@
+package com.tsong.cmall.controller.mall.param;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Author Tsong
+ * @Date 2023/4/2 23:58
+ */
+@Data
+public class UpdateUserAddressParam implements Serializable {
+    @ApiModelProperty("地址id")
+    private Long addressId;
+
+    @ApiModelProperty("用户id")
+    private Long userId;
+
+    @ApiModelProperty("收件人名称")
+    private String userName;
+
+    @ApiModelProperty("收件人联系方式")
+    private String userPhone;
+
+    @ApiModelProperty("是否默认地址 0-不是 1-是")
+    private Byte defaultFlag;
+
+    @ApiModelProperty("省")
+    private String provinceName;
+
+    @ApiModelProperty("市")
+    private String cityName;
+
+    @ApiModelProperty("区/县")
+    private String regionName;
+
+    @ApiModelProperty("详细地址")
+    private String detailAddress;
+}
