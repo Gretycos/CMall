50669a31e51c791bc03f274faa878e0728fd4f23
yaocong <rogwel@vip.qq.com>
2023-04-11 00:52:15 +0800

    web config

---
+++ src/main/java/com/tsong/cmall/config/WebMvcConfig.java
@@ -0,0 +1,56 @@
+package com.tsong.cmall.config;
+
+import com.tsong.cmall.common.Constants;
+import com.tsong.cmall.config.requestHandler.TokenToAdminUserMethodArgumentResolver;
+import com.tsong.cmall.config.requestHandler.TokenToMallUserMethodArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+/**
+ * @Author Tsong
+ * @Date 2023/4/10 18:09
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    @Autowired
+    private TokenToMallUserMethodArgumentResolver tokenToMallUserMethodArgumentResolver;
+    @Autowired
+    private TokenToAdminUserMethodArgumentResolver tokenToAdminUserMethodArgumentResolver;
+
+    /**
+     * @param argumentResolvers
+     * @tip @TokenToMallUser @TokenToAdminUser 注解处理方法
+     */
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(tokenToMallUserMethodArgumentResolver);
+        argumentResolvers.add(tokenToAdminUserMethodArgumentResolver);
+    }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**").addResourceLocations("file:" + Constants.FILE_UPLOAD_DIC);
+        registry.addResourceHandler("/goods-img/**").addResourceLocations("file:" + Constants.FILE_UPLOAD_DIC);
+
+        registry.
+                addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
+    }
+
+    /**
+     * 跨域配置
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOriginPatterns("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
